3a3a1c1b5c2ea6d4255924e9799e6959e35a40d6
Martin Franz Dallinger <martin.dallinger@sz-ybbs.ac.at>
2020-11-06 22:40:23 +0100

    Initialized Repo

---
+++ REST-Client/Model/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_Client.Model
+{
+    class Car
+    {
+        public int CarId { get; set; }
+        public string Name { get; set; }
+        public CarType Typ { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
---
+++ REST-Client/ViewModel/CarViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Rechnungsverwaltung.ViewModel;
+using REST_Client.Model;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace REST_Client.ViewModel
+{
+    
+    public class CarViewModel: INotifyPropertyChanged
+    {
+        private const string CARURL = "http://192.168.1.205:45458/api";
+        private RestClient client = new RestClient(CARURL);
+        private ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>();
+
+        public ICommand RefreshCars { get; set; }
+
+        private Visibility _loadingVisibility = Visibility.Visible;
+        public Visibility LoadingGifVisibility { get; set; }
+
+
+
+        private void RefreshInfosWithTask()
+        {
+            Task.Run(() => this.GetCarsAsync());
+        }
+
+        public CarViewModel()
+        {
+            RefreshCars = new RelayCommand(e => RefreshInfosWithTask(), e2 => true);
+            RefreshInfosWithTask();
+
+        }
+
+        private async Task GetCarsAsync()
+        {
+            LoadingVisibility = Visibility.Visible;
+
+            Cars.Clear();
+
+            RestRequest request = new RestRequest("cars", RestSharp.DataFormat.Json);
+            var response = await client.ExecuteAsync<List<Car>>(request);
+
+            LoadingVisibility = Visibility.Hidden;
+
+            response.Data?.ForEach(car => Cars.Add(car));
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Visibility LoadingVisibility
+        {
+            get
+            {
+                return _loadingVisibility;
+            }
+            set
+            {
+                if (_loadingVisibility.Equals(value))
+                    return;
+
+                _loadingVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
